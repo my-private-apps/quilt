914899c6376b19179c6dc33e98e9917f570e6c19
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-14 15:39:00 +0530

    removed the dlang source

--- source/app.d
+++
@@ -1,9 +0,0 @@
-import std.stdio;
-import arguments.parser;
-
-void main(string[] arguments)
-{
-	// Create a new argument parser and parse
-	// through the command line arguments
-	ArgumentParser parser = new ArgumentParser(arguments);
-}
--- source/utils/tests/string.d
+++
@@ -1,8 +0,0 @@
-module utils.tests.string;
-
-import utils.string;
-import std.stdio;
-
-void testStringSlice(){
-    writeln(StringUtilities.slice("Helllo", 2));
-}
